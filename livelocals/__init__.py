1dbe4dd6d6d6cdd0e40a40c4850f2687be6c006f
Christopher O'Brien <obriencj@gmail.com>
2017-10-04 11:23:38 -0400

    docstrings, and moved the interning logic to a function instead of the __new__ class method

--- livelocals/__init__.py
+++ livelocals/__init__.py
@@ -38,22 +38,35 @@ if (2, 0) <= version_info < (3, 0):
     from itertools import imap
 
 
-__all__ = ("livelocals", )
+__all__ = ("LiveLocals", "livelocals", "generatorlocals", )
 
 
-_var = namedtuple("_var", ("get_var", "set_var", "del_var", ))
+# simple way to hold the getter, setter, and clear functions for each
+# var in a frame.
+_frame_var = namedtuple("frame_var", ("get_var", "set_var", "del_var", ))
 
 
 def _create_fast_var(frame, index):
-    return _var(partial(frame_get_fast, frame, index),
-                partial(frame_set_fast, frame, index),
-                partial(frame_del_fast, frame, index))
+    """
+    Create an object with three functions for getting, setting, and
+    clearing a fast var with the given index on the specified frame.
+    """
+
+    return _frame_var(partial(frame_get_fast, frame, index),
+                      partial(frame_set_fast, frame, index),
+                      partial(frame_del_fast, frame, index))
 
 
 def _create_cell_var(frame, index):
-    return _var(partial(frame_get_cell, frame, index),
-                partial(frame_set_cell, frame, index),
-                partial(frame_del_cell, frame, index))
+    """
+    Create an object with three functions for getting, setting, and
+    clearing a cell or free var with the given index on the specified
+    frame.
+    """
+
+    return _frame_var(partial(frame_get_cell, frame, index),
+                      partial(frame_set_cell, frame, index),
+                      partial(frame_del_cell, frame, index))
 
 
 class LiveLocals(object):
@@ -61,27 +74,13 @@ class LiveLocals(object):
     Living view of a frame's local fast, free, and cell variables.
     """
 
-
     __slots__ = ("_frame_id", "_vars", "__weakref__", )
 
-    _intern = WeakValueDictionary()
 
-
-    def __new__(cls, frame=None):
-        if frame is None:
-            frame = currentframe().f_back
-
-        found = cls._intern.get(frame, None)
-        if found is None:
-            found = super(LiveLocals, cls).__new__(cls)
-            cls._intern[frame] = found
-
-        return found
-
-
-    def __init__(self, frame=None):
-        if frame is None:
-            frame = currentframe().f_back
+    def __init__(self, frame):
+        """
+        Initializes a Live Locals view for a frame.
+        """
 
         self._frame_id = id(frame)
         self._vars = vars = {}
@@ -104,18 +103,53 @@ class LiveLocals(object):
 
 
     def __getitem__(self, key):
+        """
+        Implements  `livelocals()[key]`
+
+        Returns the value of the given declared variable.
+
+        If the variable is not declared in the underlying frame,
+        raises a KeyError. If the variable is declared but not
+        currently defined, raises a NameError.
+        """
+
         return self._vars[key].get_var()
 
 
     def __setitem__(self, key, value):
+        """
+        Implements  `livelocals()[key] = var`
+
+        Assigns value to the given declared variable.
+
+        If the variable is not declared in the underlying frame,
+        raises a KeyError.
+        """
+
         return self._vars[key].set_var(value)
 
 
     def __delitem__(self, key):
+        """
+        Implements  `del livelocals()[key]`
+
+        Clears the value for the given declared variable.
+
+        If the variable is not declared in the underlying frame,
+        raises a KeyError.
+        """
+
         return self._vars[key].del_var()
 
 
     def __contains__(self, key):
+        """
+        Implements  `key in livelocals()`
+
+        True if key is declared (but not necessarily defined) in the
+        underlying frame.
+        """
+
         return key in self._vars
 
 
@@ -123,14 +157,29 @@ class LiveLocals(object):
         # Python 3 mode
 
         def keys(self):
+            """
+            Iterator of variable names with defined values in the underlying
+            frame. Omits variables which are declared but not
+            currently defined.
+            """
             return map(lambda r: r[0], self.items())
 
 
         def values(self):
+            """
+            Iterator of the values of defined variables for the underlying
+            frame.
+            """
             return map(lambda r: r[1], self.items())
 
 
         def items(self):
+            """
+            Iterator of (key, value) tuples representing the defined variables
+            for the underlying frame. Variables which are declared but
+            not set to a value (ie. declared but undefined) are
+            omitted.
+            """
             for key, var in self._vars.items():
                 try:
                     yield (key, var.get_var())
@@ -142,22 +191,45 @@ class LiveLocals(object):
         # Python 2 mode
 
         def iterkeys(self):
+            """
+            Iterator of variable names with defined values in the underlying
+            frame. Omits variables which are declared but not
+            currently defined.
+            """
             return imap(lambda r: r[0], self.iteritems())
 
 
         def keys(self):
+            """
+            List of variable names with defined values in the underlying
+            frame. Omits variables which are declared but not
+            currently defined.
+            """
             return list(self.iterkeys())
 
 
         def itervalues(self):
+            """
+            Iterator of the values of defined variables for the underlying
+            frame.
+            """
             return imap(lambda r: r[1], self.iteritems())
 
 
         def values(self):
+            """
+            List of the values of defined variables for the underlying frame.
+            """
             return list(self.itervalues())
 
 
         def iteritems(self):
+            """
+            Iterator of (key, value) tuples representing the defined variables
+            for the underlying frame. Variables which are declared but
+            not set to a value (ie. declared but undefined) are
+            omitted.
+            """
             for key, var in self._vars.iteritems():
                 try:
                     yield (key, var.get_var())
@@ -166,6 +238,12 @@ class LiveLocals(object):
 
 
         def items(self):
+            """
+            List of (key, value) tuples representing the defined variables for
+            the underlying frame. Variables which are declared but not
+            set to a value (ie. declared but undefined) are omitted.
+            """
+
             return list(self.iteritems())
 
 
@@ -209,14 +287,39 @@ class LiveLocals(object):
             return default
 
 
+# This is our default cache. Frames can't be weakreferenced, so we
+# keep a weak ref to the LiveLocals instance instead.
+_cache = WeakValueDictionary()
 
-livelocals = LiveLocals
+
+def livelocals(frame=None, _cache=_cache):
+    """
+    Given a Python frame, return a live view of its variables. If
+    frame is unspecified or None, the calling frame is used.
+    """
+
+    if frame is None:
+        frame = currentframe().f_back
+
+    if _cache is None:
+        found = LiveLocals(frame)
+
+    else:
+        found = _cache.get(frame, None)
+        if found is None:
+            found = LiveLocals(frame)
+            _cache[frame] = found
+
+    return found
 
 
 def generatorlocals(gen):
     """
-    Given a Python generator object, return a livelocals for its
-    frame.
+    Given a Python generator, return a livelocals for its frame.
+
+    Anything other than a generator object (ie. something that doesn't
+    have the gi_frame attribute) will result in an AttributeError
+    being raised.
     """
 
     return livelocals(gen.gi_frame)
